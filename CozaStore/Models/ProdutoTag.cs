332f85ef26ef42f67487dec470a1cba4b4eaf2e9
Kênia De Marchi <keniademarchi@gmail.com>
2023-10-17 12:23:49 -0300

    Finalização do banco de dados

---
+++ CozaStore/Models/ProdutoTag.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CozaStore.Models;
+
+[Table("ProdutoTag")]
+public class ProdutoTag
+{
+    [Key, Column(Order = 1)]
+    public int ProdutoId { get; set; }
+    [ForeignKey("ProdutoId")]
+    public Produto Produto { get; set; }
+
+    [Key, Column(Order = 2)]
+    public byte TagId { get; set; }
+    [ForeignKey("TagId")]
+    public Tag Tag { get; set; }
+}
